166cfde1f1f777c99e20fb2b22548081e3a64e9d
Andy Chu <andy@oilshell.org>
2018-07-22 10:10:08 -0700

    testing out trisurf

--- matplotlib/trisurf3d_demo.py
+++ matplotlib/trisurf3d_demo.py
@@ -1,3 +1,8 @@
+#!/usr/bin/python3
+
+# From
+# https://matplotlib.org/mpl_toolkits/mplot3d/tutorial.html#surface-plots
+
 '''
 ======================
 Triangular 3D surfaces
@@ -24,11 +29,34 @@ angles = np.repeat(angles[..., np.newaxis], n_radii, axis=1)
 # Convert polar (radii, angles) coords to cartesian (x, y) coords.
 # (0, 0) is manually added at this stage,  so there will be no duplicate
 # points in the (x, y) plane.
-x = np.append(0, (radii*np.cos(angles)).flatten())
-y = np.append(0, (radii*np.sin(angles)).flatten())
+if 0:
+  x = np.append(0, (radii*np.cos(angles)).flatten())
+  y = np.append(0, (radii*np.sin(angles)).flatten())
 
 # Compute z to make the pringle surface.
-z = np.sin(-x*y)
+  z = np.sin(-x*y)
+else:
+  points = np.array([
+    [0, 0, 0],
+    [0, 1, 0],
+    [1, 1, 0],
+    #[1, 1, 1],
+    [2, 2, 2],
+  ])
+
+  print(points)
+  print(points.shape)
+
+  x = points[:, 0]
+  y = points[:, 1]
+  z = points[:, 2]
+
+  print(x)
+  print(y)
+  print(z)
+
+#import sys
+#sys.exit()
 
 fig = plt.figure()
 ax = fig.gca(projection='3d')
