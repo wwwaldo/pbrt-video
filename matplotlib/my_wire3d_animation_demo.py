1778bbdb7ef6762cf8b71736617177a067bf31ad
Andy Chu <andy@oilshell.org>
2018-07-25 03:26:04 -0700

    pause

--- matplotlib/my_wire3d_animation_demo.py
+++ matplotlib/my_wire3d_animation_demo.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 """
 ==========================
 Rotating 3D wireframe plot
@@ -22,28 +23,34 @@ def generate(X, Y, phi):
     return np.cos(2 * np.pi * X + phi) * R
 
 
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
+def main():
+  fig = plt.figure()
+  ax = fig.add_subplot(111, projection='3d')
 
 # Make the X, Y meshgrid.
-xs = np.linspace(-1, 1, 50)
-ys = np.linspace(-1, 1, 50)
-X, Y = np.meshgrid(xs, ys)
+  xs = np.linspace(-1, 1, 50)
+  ys = np.linspace(-1, 1, 50)
+  X, Y = np.meshgrid(xs, ys)
 
 # Set the z axis limits so they aren't recalculated each frame.
-ax.set_zlim(-1, 1)
+  ax.set_zlim(-1, 1)
 
 # Begin plotting.
-wframe = None
-tstart = time.time()
-for phi in np.linspace(0, 180. / np.pi, 100):
-    # If a line collection is already remove it before drawing.
-    if wframe:
-        ax.collections.remove(wframe)
+  wframe = None
+  tstart = time.time()
+  for phi in np.linspace(0, 180. / np.pi, 100):
+      # If a line collection is already remove it before drawing.
+      if wframe:
+          ax.collections.remove(wframe)
 
-    # Plot the new wireframe and pause briefly before continuing.
-    Z = generate(X, Y, phi)
-    wframe = ax.plot_wireframe(X, Y, Z, rstride=2, cstride=2)
-    plt.pause(.001)
+      # Plot the new wireframe and pause briefly before continuing.
+      Z = generate(X, Y, phi)
+      wframe = ax.plot_wireframe(X, Y, Z, rstride=2, cstride=2)
 
-print('Average FPS: %f' % (100 / (time.time() - tstart)))
+      # Hm this is needed to show anything!  That was easy.
+      plt.pause(.001)
+
+  print('Average FPS: %f' % (100 / (time.time() - tstart)))
+
+
+main()
