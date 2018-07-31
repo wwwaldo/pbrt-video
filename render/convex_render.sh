4479777d66855f63bf1c6cdcda850a013f9c034c
Andy Chu <andy@oilshell.org>
2018-07-31 17:49:17 -0400

    run convex_render on broome

--- render/convex_render.sh
+++ render/convex_render.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+set -o nounset
+set -o pipefail
+set -o errexit
+
 # Runs generate_ply and renders the resulting convex set in pbrt.
 # Output is saved to temp.png.
-PBRT=/home/caroline/recurse/pbrt-v3/build_debug/pbrt
+if test $USER = 'andy'; then
+  PBRT=/home/andy/git/other/pbrt-v3-build/pbrt
+else
+  PBRT=/home/caroline/recurse/pbrt-v3/build_debug/pbrt
+fi
 python3 generate_ply.py temp.ply
 $PBRT convex_render.pbrt
