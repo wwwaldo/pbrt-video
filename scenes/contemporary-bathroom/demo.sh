9fca5a9f87f6921554f2164130676831604a4631
Caroline Lin <ruoyi.lin@gmail.com>
2018-08-07 17:29:38 -0400

    Add performance flags to demo.sh

--- scenes/contemporary-bathroom/demo.sh
+++ scenes/contemporary-bathroom/demo.sh
@@ -14,4 +14,16 @@ demo() {
  $pbrt bathroom-demo.pbrt 
 }
 
+low() {
+  ./temp.py --resx 400 --resy 400 --nsamples 32 --depth 3 # 10 sec render / frame
+}
+
+medium(){
+  ./temp.py --resx 500 --resy 500 --nsamples 128 --depth 6 # 2 min render / frame 
+}
+
+high(){
+  ./temp.py --resx 1000 --resy 1000 --nsamples 512 --depth 8 # ?? / frame
+}
+
 "$@"
